7a696c13329589b797e7a72d45eccb61e74b4223
pbrehill <pbrehill@gmail.com>
2022-10-09 20:18:33 +1100

    Added in unique sessions hopefully

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from chat import ChatBot
 from flask import Flask, render_template, request, session
-from flask.ext.session import Session
+# from flask_session import Session
 import pandas as pd
+import random
+import secrets
+
 app = Flask(__name__)
 
 v_prompt = "VidyaBot is a chatbot that is the digital recreation of a dead woman designed to help her friends grieve, she is upbeat but spooky.\n"
-comps = pd.read_pickle('/home/pbrehill/VidyaBot/VidyaBot2/embeddings.pkl')
+# comps = pd.read_pickle('/home/pbrehill/VidyaBot/VidyaBot2/embeddings.pkl')
+comps = pd.read_pickle('embeddings.pkl')
 
-chatbot = ChatBot(v_prompt, comps)
+secret = secrets.token_urlsafe(32)
 
-SESSION_TYPE = 'redis'
-Session(app)
+app.secret_key = secret
+
+chatbot = ChatBot(comps)
+
+# SESSION_TYPE = 'redis'
+# Session(app)
 
 @app.route("/")
 def home():
     session['key'] = "".join([random.choice('0123456789ABCDEF') for i in range(16)])
-    session['v_prompt'] = v_prompt
+    session['orig_prompt'] = v_prompt
     session['prompt'] = v_prompt
+    session['i'] = 0
     return render_template("index.html")
 
 @app.route("/get")
-def get_bot_response(chatbot):
+def get_bot_response():
     userText = request.args.get('msg')
-    resp = str(chatbot.get_replysession['prompt'] + (userText))
-    session['prompt'] = resp
+    resp, prompt = chatbot.get_reply(session['prompt'] + '\nHuman: ' + (userText), session['orig_prompt'], session['i'])
+    session['prompt'] = prompt
+    session['i'] += 1
     return resp.replace("VidyaBot: ", "")
 
 if __name__ == "__main__":
--- chat.py
+++ chat.py
@@ -9,29 +9,27 @@ import re
 
 
 class ChatBot:
-    def __init__(self, prompt_orig, comparisons):
+    def __init__(self, comparisons):
         openai.api_key = os.environ["OPENAI_API_KEY"]
-        self.prompt_orig = prompt_orig
-        self.prompt = prompt_orig
         self.comparisons = comparisons
         self.i = 0
         self.ask_for_delete = random.randint(3,10)
 
-    def get_reply(self, txt_input):
+    def get_reply(self, txt_input, prompt_orig, i):
         # Truncate prompt if needed
-        if len(self.prompt) > 1500:
-            self.prompt = self.prompt[:600] + self.prompt_orig + '\n'
+        if len(txt_input) > 2000:
+            txt_input = txt_input[-1200:] + prompt_orig + '\n'
 
-        self.prompt += 'Human: ' + txt_input + '\nVidyaBot: '
+        txt_input += '\nVidyaBot: '
 
-        if self.i == self.ask_for_delete:
+        if i == self.ask_for_delete:
             response_txt = "i actually have something to ask you, im finding being a bot a bit weird. would you delete me pls?\n"
-        elif self.i == 2 * self.ask_for_delete:
+        elif i == 2 * self.ask_for_delete:
             response_txt = "oh have you forgotten how to delete me?\n"
         else:
             response = openai.Completion.create(
               model="text-curie-001",
-              prompt=self.prompt,
+              prompt=txt_input,
               temperature=0.5,
               max_tokens=1000,
               top_p=0.5,
@@ -48,19 +46,16 @@ class ChatBot:
         distance = [np.linalg.norm(np.array(row) - np.array(result)) for row in np.array(self.comparisons.embeddings)]
         choice_index = np.argmin(distance)
         best_match = self.comparisons.tweet.iloc[choice_index]
-        min_dist = distance[choice_index]
         self.comparisons.drop(self.comparisons.index[choice_index], inplace = True)
 
         full_response = response_txt + ' ' + best_match
-        full_response = 'VidyaBot: ' + full_response.lower().replace("you", "u").replace("you", "u")
+        full_response = full_response.lower().replace("you", "u").replace("you", "u")
 
         if "human:" in full_response:
             full_response = full_response.split('human:', 1)[0]
 
-        self.prompt += full_response
+        new_prompt = txt_input + full_response
 
-        self.i += 1
-
-        return full_response
+        return full_response, new_prompt
 
 
